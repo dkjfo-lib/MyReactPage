2fb95638ef7a2396e8058044ee507e684eb16ade
Mike <mihailfd@gmail.com>
2022-05-29 18:31:33 +0300

    290522

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,24 @@
-import GoalsList from "./GoalsList"
+import GoalsList from "./GoalsList";
 
-export default function Main(){
-    return(
-        <div>
-            <GoalsList/>
+export default function Main() {
+  return (
+    <div>
+      <GoalsList />
+      <div className="Recommendations">
+        <h2>Sites I recommend❤️</h2>
+        <div className="Recommendations-list">
+          <a href="https://moviesjoy.to/" target="_blank" rel="noreferrer">
+            Films/TV Shows
+          </a>
+          <a
+            href="https://en.wikiversity.org/wiki/Category:Introduction_to_Italian"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Italian
+          </a>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
