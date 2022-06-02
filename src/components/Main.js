e68316ae881ab0b4018cc8a409151078209444ad
Mike <mihailfd@gmail.com>
2022-06-02 20:51:15 +0300

    guiding stones and gitignore update

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,6 +11,34 @@ function Main() {
         <GoalsList />
         <Recommendations />
         <Improvements />
+        <div>
+          <h4>
+            Ибо мир хороший и Бог хороший 
+            все что остается это тебе быть хорошим
+            
+            For the World is good and the God is good 
+            the only thing remaining is for yourself to be good
+
+            "Do not act as if you were going to live ten thousand years.
+            Death hangs over you. While you live, while it is in your power, be good"
+
+            "Let no act be done without a purpose" - Marcus
+
+            Keep your life simple and do not endue in pleasures
+            For death and old age will conquer your soul some day
+
+            "Take away your opinion and then there is taken away the complaint, "I have been harmed". 
+            Take away the complaint, "I have been harmed", and the harm is taken away"
+            - Marcus 
+
+            Do not let thoughts about past or future distract you, for neither exists.
+            The only thing you have is present and thus, use it and enjoy it
+
+            Keep your thoughts simple and let them have purpose. 
+            Do not think what others say or think 
+            for life is an opinion, and it is up to you to have your own
+          </h4>
+        </div>
       </div>
     </div>
   );
